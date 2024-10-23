46c285234e57522c8fea4db31214be19ae9c3037
Anais <anais.fonteneau@etu.u-bordeaux.fr>
2024-10-23 22:42:37 +0200

    initialisation du projet

---
+++ src/main/java/com/bdx/anais/ApplicationRecetteCuisine/ApplicationRecetteCuisineApplication.java
@@ -0,0 +1,13 @@
+package com.bdx.anais.ApplicationRecetteCuisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRecetteCuisineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationRecetteCuisineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdx/anais/ApplicationRecetteCuisine/ApplicationRecetteCuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdx.anais.ApplicationRecetteCuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationRecetteCuisineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
