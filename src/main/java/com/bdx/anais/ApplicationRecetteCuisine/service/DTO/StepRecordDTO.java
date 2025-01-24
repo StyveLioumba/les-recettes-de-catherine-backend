4719bd5dff642e7c48197f93b004e4207066a576
styve Lioumba <styve.lioumba@jdc.fr>
2025-01-24 16:11:32 +0100

    mise a jour de la responseÃ

--- src/main/java/com/bdx/anais/ApplicationRecetteCuisine/service/DTO/StepRecordDTO.java
+++ src/main/java/com/bdx/anais/ApplicationRecetteCuisine/service/DTO/StepRecordDTO.java
@@ -1,33 +1,14 @@
 package com.bdx.anais.ApplicationRecetteCuisine.service.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class StepRecordDTO {
 
     private Integer numero_etape;
     private String idRecette;
     private String description;
 
-    public String getIdRecette() {
-        return idRecette;
-    }
-
-    public void setIdRecette(String idRecette) {
-        this.idRecette = idRecette;
-    }
-
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getNumero_etape() {
-        return numero_etape;
-    }
-
-    public void setNumero_etape(Integer numero_etape) {
-        this.numero_etape = numero_etape;
-    }
 }
